4ed24d8442ccc08dbd3ef27959bebf238b0056dd
ayushstwt <ayusrivastava2k3@gmail.com>
2024-09-03 20:13:55 +0530

    initial commint

---
+++ src/main/java/com/tier3Hub/event_scheduler_application/EventSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tier3Hub.event_scheduler_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tier3Hub/event_scheduler_application/EventSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tier3Hub.event_scheduler_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
